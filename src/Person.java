3d3ce68c32f668f97896adda2002c1c7caa88564
ik013043z1 <ik013043z1@H013043.NET>
2018-11-30 11:37:26 +0100

    1.0

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 
  */
@@ -12,7 +14,7 @@ public class Person {
 	private int age = 0;
 	private int weight = 0;
 	private String dni = "";
-private int height = 0;
+	private int height = 0;
 
 	public Person(String name, int age, int weight, int height, String dni) {
 
@@ -36,7 +38,7 @@ private int height = 0;
 		this.weight = weight;
 
 	}
-	
+
 	public void setName(String name) {
 
 		this.name = name;
@@ -67,7 +69,10 @@ private int height = 0;
 
 	public void setWeight(int weight) {
 
-		this.weight = weight;
+		if (weight > 5 && weight < 400) {
+
+			this.weight = weight;
+		}
 
 	}
 
@@ -86,34 +91,34 @@ private int height = 0;
 		}
 
 	}
-	
+
 	public String getDni() {
 
 		return dni;
 
 	}
-	
-	public void setheight(int height) {
-		
+
+	public void setHeight(int height) {
+
 		if (height > 30 && height < 220) {
 
-		this.height = height;
+			this.height = height;
 		}
 
 	}
 
-	public int getheight() {
+	public int getHeight() {
 
 		return weight;
 
 	}
-	
+
 	/**
 	 * This method will return true if the weight is ideal and false if it is not.
 	 * To calculate this i use the formula weight(Kg)/height^2(m) were each value
 	 * between 18 and 24 will be considered a good one
 	 **/
-	public boolean idealWeight(int weight) {
+	public boolean idealWeight(int weight, int height) {
 
 		if (18 < weight / (height / 100) ^ 2 < 24)
 			return true;
@@ -121,30 +126,43 @@ private int height = 0;
 			return false;
 
 	}
-	
-	/** This method will print all the info about the person object**/
-	public String  toString() {
-		
-		return "Name: " +name+ " Age: "+ Integer.toString(age) + " Weight: " + Integer.toString(weight)+ " DNI: " +dni+ " Height: " + Integer.toString(height);
-		
-	}
-	
-	/**
-	 **/
-	
-	public String toStringData() {
-		
-		if (dni.matches("")) {
-			
 
-				
-				this.dni=Double.toString(Math.random() * (99999999 - 0) + 0) + ;
-				
-		
-			
-		}
-		
+	/** This method will print all the info about the person object **/
+	public String toString() {
+
+		return "Name: " + name + " Age: " + Integer.toString(age) + " Weight: " + Integer.toString(weight) + " DNI: "
+				+ dni + " Height: " + Integer.toString(height);
+
 	}
-	 
+
+	/**
+	 * This method will create a String with a random number and a random letter
+	 **/
+
+	public void dniByDefault() {
+		
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		if (dni.matches("")) {
+
+			Random r = new Random();
+
+			this.dni = Double.toString(Math.random() * (99999999 - 00000000) + 00000000)
+					+ alphabet.charAt(alphabet.charAt(r.nextInt(alphabet.length())));
+
+		}
+
+	}
+
+	/** This method sees if the age is urder 18 or not **/
+	public boolean isAdult() {
+
+		if (age < 18)
+
+			return false;
+		else
+			return true;
+
+	}
 
 }
